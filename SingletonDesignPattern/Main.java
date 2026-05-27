699db67e2ef223e75287d7117135854d147a7989
Rohit Rai <rohitrai170904@gmail.com>
2026-05-28 00:58:46 +0530

    Optimzed Synchronization

--- SingletonDesignPattern/Main.java
+++ SingletonDesignPattern/Main.java
@@ -22,6 +22,29 @@ class Singleton{
     }
 }
 
+// This is a better Synchronization approach  as we are only synchronizing if the instance has not been created if it already exists then ther eis no need ofr that 
+// also double check is there becaluse 2 parallal thread can  have instance == null and then one gets locked and then again if no check is done then new  isntance may get created 
+
+class OptimizedSingleton {
+
+    private static  OptimizedSingleton instance ; 
+    private OptimizedSingleton(){}
+
+    public static OptimizedSingleton getInstance(){
+
+        if ( instance == null ) {
+
+            synchronized(OptimizedSingleton.class){
+
+                if ( instance == null ) {
+                    instance = new OptimizedSingleton() ; 
+                }
+            }
+        }
+        return instance ;
+    }
+}
+
 
 
 
@@ -32,9 +55,14 @@ public class Main {
 
         Singleton s1 = Singleton.getInstance() ; 
         Singleton s2 = Singleton.getInstance() ; 
+
+        OptimizedSingleton o1 = OptimizedSingleton.getInstance() ; 
+        OptimizedSingleton o2 = OptimizedSingleton.getInstance() ; 
         
         System.out.println(s1);
         System.out.println(s2);
+        System.out.println(o1);
+        System.out.println(o2);
 
         
     }
